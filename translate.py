c24665dc9c8f12a684893eabbdaa4a28963c68ed
Sergey Simonov <lex_ionov@mail.ru>
2020-02-23 14:46:59 +0300

    additional functions added

--- translate.py
+++ translate.py
@@ -1,23 +1,29 @@
 import requests
+import os
 from datetime import datetime
 
 
 def reader():
-    file = input('Введите название файла, из которого будем брать текст:\n>')
-    with open(file, encoding='utf8') as data:
+    file = input('Введите путь файла, из которого будем брать текст:\n>')
+    path = os.path.join(file)
+    print(path, 'test')
+    with open(path, encoding='utf8') as data:
         text = data.read()
     from_lang = input('С какого языка переводить? (de, ru, es...)\n>').lower()
     to_lang = input('На какой язык переводим?\n>').lower()
+    result_path = input('Введите путь, куда сохранить файл: ')
     result_name = f'result-{from_lang}-{to_lang}-{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.txt'
-    with open(result_name, 'w', encoding='utf8') as result:
-        translated_text = translate_it(text, from_lang, to_lang)
+    full_path = os.path.join(result_path, result_name)
+    print(full_path, 'test')
+    with open(full_path, 'w', encoding='utf8') as result:
+        translated_text = translate_it(text, from_lang, to_lang='ru')
         result.write(translated_text)
     print(f'Результат перевода:\n{translated_text}')
-    return result_name
+    return full_path
 
 
 def translate_it(text, from_lang, to_lang):
-    api_key = 'your_token_here'
+    api_key = 'trnsl.1.1.20200222T173117Z.44c78eecca664b79.27fcef98acd8ece997fa0520fec7fa6f081291f1'
     url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
     params = {
         'key': api_key,
@@ -35,7 +41,7 @@ def translate_it(text, from_lang, to_lang):
 
 
 def uploader(file):
-    token = 'OAuth your_token_here'
+    token = 'OAuth AgAAAAAgRZ8KAADLW-EJI3AA-EswnWkq2QpgWDo'
     url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
     headers = {
         'Authorization': token
